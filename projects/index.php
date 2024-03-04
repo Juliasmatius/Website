c1bfe4538df68d99329ec016f04f68e6f4307ce8
Juli The Furry <julinlenovo@gmail.com>
2024-03-04 08:35:00 +0000

    yeowch

--- projects/index.php
+++ projects/index.php
@@ -28,8 +28,19 @@
 
   <div class="main">
     <div class="page_text_left">
-    I have made a few actually useful scripts like Portal 2 VR mod manager and Portal 2 VR installer. Most of my minor projects are on my github or replit, some projects will me on my itch.<br>
-    Major projects will be listed below
+    <p>I have made a few actually useful scripts like Portal 2 VR mod manager and Portal 2 VR installer. Most of my minor projects are on my github or replit, some projects will me on my itch.<br>
+    Major projects will be listed below</p><br>
+
+    <details>
+      <summary>My game</summary>
+      <h2>My game</h2>
+      <p>Not out yet, and I still don't have a name.
+
+      This is going to released on my itch.io. It's going to be a platformer/puzzle game.
+
+
+      Want to know more about it? Join the newsletter on the right! Don't worry, I will not spam your or sell your email.</p>
+    </details>
     </div>
     <div class="page_right">
 
