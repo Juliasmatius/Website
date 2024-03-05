42d8cb717a30aa8dfd0f645287f897c7a706ffbb
Juli The Furry <julinlenovo@gmail.com>
2024-03-05 10:21:14 +0000

    yeeeeeeeeeeeeeeeeeeeee4eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- globals.js
+++ globals.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', function () {
-
   const imgage = document.getElementById("stealingTheft");
   imgage.addEventListener("click", function(){ window.open("/yons_crazybox/insanity.mp4");; });
 })
