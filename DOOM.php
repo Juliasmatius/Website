8a470eeb9c87586e1d89a953a037fdd7302c2721
Juli The Furry <julinlenovo@gmail.com>
2024-03-05 17:53:17 +0000

    added DOOM

--- DOOM.php
+++ DOOM.php
@@ -1,13 +1,15 @@
- <audio controls>
+ <audio id="doom" controls>
   <source src="/DOOM.mp3" type="audio/mpeg">
 Your browser does not support the audio element.
 </audio> 
-<iframe src="/" width="100%" height="100%"></iframe>
+<iframe id="frame" src="/" width="100%" height="100%"></iframe>
 
 <style>
   body {
     background-color: rgb(0, 27, 41);
     overflow: hidden;
+    justify-content: center;
+    align-items: center;
     align-content: center;
   }
   iframe {
@@ -16,3 +18,32 @@ Your browser does not support the audio element.
     height: 100%;
   }
 </style>
+
+<script>
+  const audio = document.getElementById('doom');
+
+  // Play the audio
+  function playAudio() {
+      audio.play();
+  }
+  document.addEventListener("click", function(event) {
+      // When any part of the page is clicked, this function will be called
+    playAudio();
+      // You can perform any action you want here, like showing a message or triggering another function
+  });
+
+  var iframe = document.getElementById("frame");
+
+  // Wait for the iframe to load
+  iframe.onload = function() {
+      // Access the content window of the iframe
+      var iframeDocument = iframe.contentWindow.document;
+
+      // Add an event listener to the iframe document to detect clicks
+      iframeDocument.addEventListener("click", function(event) {
+          // When any part of the iframe document is clicked, this function will be called
+        playAudio();
+          // You can perform any action you want here, like showing a message or triggering another function
+      });
+  };
+</script>
