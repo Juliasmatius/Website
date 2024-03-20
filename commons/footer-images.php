f38aa9af2a3af6578fe03c66785339b3000a90be
Juli The Furry <julinlenovo@gmail.com>
2024-03-20 07:31:12 +0000

    DDeeipjsdgiiojdgiohdsgisfgfioj

---
+++ commons/footer-images.php
@@ -0,0 +1,11 @@
+<style>
+  body {
+    content-align: center;
+  }
+</style>
+  <img src="/buttons/csshard.gif">
+  <img src="/buttons/linux.gif">
+  <img src="/buttons/paws.gif">
+  <img src="/buttons/php.gif">
+  <img src="/buttons/powered_linux.gif">
+  <img src="/buttons/ubuntu.gif">
--- commons/footer.php
+++ commons/footer.php
@@ -1,11 +1,13 @@
 
 <link rel="stylesheet" href="/commons/style.css">
 <div class="footer">
-  <p>&copy; <?php echo date(
+  <div class="footer_text">
+    <p>&copy; <?php echo date(
       "Y"
   ); ?> Juli <a href="/images/footer.png">&</a> <a href="/lexis_crazybox/thesilly.html">idiot</a>. All rights reserved.</p>
-  <div class="images">
-
+  </div>
+    <div class="footer_images">
+    <?php include "footer-images.php";?>
   </div>
 </div>
 </body>
