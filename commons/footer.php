4ccf5ad0dcac3271bdf9f2e39da0bf67a3372b57
Juli The Furry <julinlenovo@gmail.com>
2024-03-20 11:02:06 +0000

    better footer

--- commons/footer.php
+++ commons/footer.php
@@ -1,14 +1,19 @@
-
 <link rel="stylesheet" href="/commons/style.css">
 <div class="footer">
-  <div class="footer_text">
+  <div class="footer_text"
     <p>&copy; <?php echo date(
       "Y"
   ); ?> Juli <a href="/images/footer.png">&</a> <a href="/lexis_crazybox/thesilly.html">idiot</a>. All rights reserved.</p>
-  </div>
+</div>
     <div class="footer_images">
-    <?php include "footer-images.php";?>
+      <img src="/buttons/csshard.gif">
+      <img src="/buttons/linux.gif">
+      <img src="/buttons/paws.gif">
+      <img src="/buttons/php.gif">
+      <img src="/buttons/powered_linux.gif">
+      <img src="/buttons/ubuntu.gif">
   </div>
 </div>
+
 </body>
 </html>
