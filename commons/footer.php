1e53dd4f3e364c868036dec31872e36fa1c65fe2
Juli The Furry <julinlenovo@gmail.com>
2024-03-20 17:48:33 +0000

    say gex

--- commons/footer.php
+++ commons/footer.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="/commons/style.css">
+<img id=adolf src="/icons/twitter_better.svg">
 <div class="footer">
   <div class="footer_text"
     <p>&copy; <?php echo date(
@@ -6,12 +7,12 @@
   ); ?> Juli <a href="/images/footer.png">&</a> <a href="/lexis_crazybox/thesilly.html">idiot</a>. All rights reserved.</p>
 </div>
     <div class="footer_images">
-      <img src="/buttons/csshard.gif">
-      <img src="/buttons/linux.gif">
-      <img src="/buttons/paws.gif">
-      <img src="/buttons/php.gif">
-      <img src="/buttons/powered_linux.gif">
-      <img src="/buttons/ubuntu.gif">
+      <a href="https://www.codecademy.com/learn/learn-css"><img src="/buttons/csshard.gif"></a>
+      <a href="https://www.linux.org/"><img src="/buttons/linux.gif"></a>
+      <a href='javascript:alert("furrynism")'><img src="/buttons/paws.gif">
+      <a href="https://www.php.net/"><img src="/buttons/php.gif"></a>
+      <a href="https://www.linux.org/"><img src="/buttons/powered_linux.gif"></a>
+      <a href="https://ubuntu.com/"><img src="/buttons/ubuntu.gif"></a>
   </div>
 </div>
 
