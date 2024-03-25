cab98cd350b12c1124fc6399c27a0565fb00f951
Juli The Furry <julinlenovo@gmail.com>
2024-03-25 06:35:17 +0000

    this probably doesnt work

--- yons_crazybox/thesilly.js
+++ yons_crazybox/thesilly.js
@@ -122,3 +122,40 @@ window.addEventListener('load', function() {
     window.location.href = "/yons_crazybox/insanity.mp4";;
   });
 })
+
+
+$apiKey = "f27b0fc77baba3236a3876f57446d679";
+$apiRoot = "http://ws.audioscrobbler.com/2.0/";
+$username = "fake_yon";
+
+// URL to make the request to
+$url = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=fake_yon&format=json&limit=1&api_key=f27b0fc77baba3236a3876f57446d679";
+function fetchRecentTrack() {
+  console.log("Update")
+  fetch($url)
+    .then(response => response.json())
+    .then(data => {
+      const recentTrack = data.recenttracks.track[0];
+      const artist = recentTrack.artist["#text"];
+      const track = recentTrack.name;
+      const url = recentTrack.url;
+      const nowPlaying = recentTrack["@attr"] && recentTrack["@attr"].nowplaying;
+
+      const currListenElement = document.getElementById('currlisten');
+      const trackInfoElement = document.getElementById('trackinfo');
+
+      if (nowPlaying) {
+        currListenElement.innerHTML = `Currently listening to<br><a id="trackinfo" href="${url}"><b>${artist}</b> - ${track}</a>`;
+      } else {
+        currListenElement.innerHTML = '<br>';
+        trackInfoElement.href = '';
+      }
+    })
+    .catch(error => console.error('Error fetching recent tracks:', error));
+}
+
+// Call fetchRecentTrack initially
+fetchRecentTrack();
+
+// Refresh every 5 seconds
+setInterval(fetchRecentTrack, 15000);
