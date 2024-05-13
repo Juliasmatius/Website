a72790835048f55f98456c97794ecabecb687740
Yon <161496632+y0nkagor@users.noreply.github.com>
2024-05-13 13:09:01 +0000

    Added John Marston

--- yons_crazybox/thesilly.js
+++ yons_crazybox/thesilly.js
@@ -2,6 +2,7 @@ let counter = 0;
 let counter2 = 0;
 let counter3 = 0;
 let counter4 = 0;
+let counter5 = 0;
 
 function eternalDamnation() {
 
@@ -107,6 +108,46 @@ function eternalDamnation4() {
   // Increment the counter and update the output
   counter4++;
   document.getElementById('output4').innerHTML = counter4;
+}
+
+window.addEventListener('load', function() {
+  const imgage = document.getElementById("stealingTheft");
+  imgage.addEventListener("click", function() {
+    window.location.href = "/yons_crazybox/insanity.mp4";;
+  });
+})
+
+window.addEventListener('load', function() {
+  const imgage = document.getElementById("stealingTheft2");
+  imgage.addEventListener("click", function() {
+    window.location.href = "/yons_crazybox/insanity.mp4";;
+  });
+})
+
+function eternalDamnation5() {
+  document.getElementById("counter_text").style.display = "block";
+
+  const videoContainer = document.createElement("div");
+  videoContainer.style.display = "inline-block";
+  document.body.appendChild(videoContainer);
+
+  const video = document.createElement("video");
+  video.width = 560;
+  video.height = 315;
+  video.id = "sillyvideo5" + counter5; // Unique ID for each video
+  video.autoplay = true;
+  video.loop = true;
+  video.playbackRate = 1; // You can set different playback rate for the second video
+  videoContainer.appendChild(video);
+
+  const source = document.createElement("source");
+  source.src = "/yons_crazybox/GAY.mp4"; // Change the source path to your video
+  source.type = "video/mp4";
+  video.appendChild(source);
+
+  // Increment the counter and update the output
+  counter5++;
+  document.getElementById('output5').innerHTML = counter5;
 }
 
 window.addEventListener('load', function() {
