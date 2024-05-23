5bce67bc96b9323625df45984a513b5236cff477
Juli <julinlenovo@gmail.com>
2024-05-23 07:26:20 +0000

    added coding platform icons

--- projects/index.php
+++ projects/index.php
@@ -6,7 +6,11 @@
     <div class="page_text_left">
     <p>I have made a few actually useful scripts like Portal 2 VR mod manager and Portal 2 VR installer. Most of my minor projects are on my github or replit, some projects will me on my itch.<br>
     Major projects will be listed below</p><br>
-      
+    <div class="socials">
+      <a alt="Github" target="_blank" href="https://github.com/Juliasmatius"><img alt="Github" title="Github" width=50px height=50px src="/static/icons/github.svg"/></img></a>
+      <a alt="Itch" target="_blank" href="https://julimiro.itch.io/"><img alt="Itch" title="Itch.io" width=50px height=50px src="/static/icons/itch.svg"/></img></a>
+      <a alt="Replit" target="_blank" href="https://replit.com/@Julimiro"><img alt="Replit" title="Replit" width=50px height=50px src="/static/icons/replit.svg"/></img></a>
+    </div>
     <details>
       <summary>My game</summary>
       <h2>My game</h2>
