799258b45312631d09824d35b09e71e37afb6abb
Juli <julinlenovo@gmail.com>
2024-06-13 14:33:51 +0000

    misc updates

--- static/commons/footer.php
+++ static/commons/footer.php
@@ -13,8 +13,10 @@
       <a target="_blank"href="https://www.codecademy.com/learn/learn-css"><img src="/static/buttons/csshard.gif"></a>
       <a target="_blank"href="https://www.linux.org/"><img src="/static/buttons/linux.gif"></a>
       <a href='javascript:alert("Got ideas where I should point this to?")'><img src="/static/buttons/paws.gif">
-      <a target="_blank"href="https://www.php.net/"><img src="/static/buttons/php.gif"></a>
+      <a target="_blank" href="https://www.php.net/"><img src="/static/buttons/php.gif"></a>
       <a target="_blank" href="https://www.linux.org/"><img src="/static/buttons/powered_linux.gif"></a>
+      <a href='javascript:alert("Got ideas where I should point this to?")'><img src="/static/buttons/transrights.gif"></a>
+      <a href='javascript:alert("Got ideas where I should point this to?")'><img src="/static/buttons/gayweb.gif"></a>
       <!-- <a target="_blank" href="https://ubuntu.com/"><img src="/static/buttons/ubuntu.gif"></a> -->
   </div>
 </div>
