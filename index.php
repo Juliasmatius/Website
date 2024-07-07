5d3fd805304f3bdc4a761112a6ec8dc362043968
Juli <julinlenovo@gmail.com>
2024-07-07 20:06:06 +0000

    meow

--- index.php
+++ index.php
@@ -21,17 +21,16 @@ $date = calculate_elapsed_years("2009-08-29");
      </div>
     <div class="main_text">
       <h2>Hello!</h2>
-      <p>I am Juli a Finnish pansexual gender unsure furry from '09(thats <?php echo $date?> years old). I go by Juli or Julimiro, any pronouns are fine.</p></p><br>
+      <p>I am Juli, a Finnish pansexual gender unsure furry from '09(thats <?php echo $date?> years old). I go by Juli or Julimiro, any pronouns are fine.</p></p><br>
       <p>I have a few coding projects which are on my github and replit. I am also working on a game, for more info visit <a href="/projects/">projects</a>.</p><br>
       <p>My socials can be found below</p>
       <div class="socials">
         <a alt="twitter" target="_blank" href="https://twitter.com/julithefurry"><img alt="twitter" title="Twitter" id="twitter" width=50px height=50px src="/static/icons/twitter.svg"/></img></a>
-        <a alt="mastadon" target="_blank" href="https://eepykitty.lol/@juli"><img alt="mastodon" title="Mastodon" width=50px height=50px src="/static/icons/mastodon.svg"/></img></a>
-        <a alt="Last.fm" target="_blank" href="https://www.last.fm/user/Julimiro/listening-report/week"><img alt="Last.fm" title="Last.fm" width=50px height=50px src="/static/icons/lastfm.svg"/></img></a>
-        <a alt="Reddit" target="_blank" href="https://www.reddit.com/user/Julis_texsture_team/"><img alt="Reddit" title="Reddit" width=50px height=50px src="/static/icons/reddit.svg"/></img></a>
+        <a alt="Fediverse" target="_blank" href="https://eepykitty.lol/@juli"><img alt="Fediverse" title="Fediverse" width=50px height=50px src="/static/icons/fediverse.svg"/></img></a>
+        <a alt="Last.fm" target="_blank" href="https://www.last.fm/user/Julimiro"><img alt="Last.fm" title="Last.fm" width=50px height=50px src="/static/icons/lastfm.svg"/></img></a>
+        <a alt="Lemmy" target="_blank" href="https://lemmy.blahaj.zone/u/Juli"><img alt="Lemmy" title="Lemmy" width=50px height=50px src="/static/icons/lemmy.svg"/></img></a>
         <a alt="Steam" target="_blank" href="https://steamcommunity.com/id/julimiro/"><img alt="Steam" title="Steam" width=50px height=50px src="/static/icons/steam.svg"/></img></a>
         <a alt="Youtube" target="_blank" href="https://www.youtube.com/@JuliTheFurry"><img alt="Youtube" title="Youtube" width=50px height=50px src="/static/icons/youtube.svg"/></img></a>
-        <a alt="Discord" target="_blank" href="https://discord.com/users/1043510819859730533"><img alt="Discord" title="Discord" width=50px height=50px src="/static/icons/discord.svg"/></img></a>
       </div>
     
        
